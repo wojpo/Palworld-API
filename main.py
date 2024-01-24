28f32eb5ff90b046ab335ba9f96ceeaea1f6c76c
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-01-24 22:25:49 +0100

    Huge

--- main.py
+++ main.py
@@ -1,7 +1,104 @@
 from fastapi import FastAPI
+from bs4 import BeautifulSoup
+import requests
 
 app = FastAPI()
 
-@app.get("/")
+
+@app.get("/Pal")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    global PalNumber, PalElement, PalDrops, paldrop1, paldrop2
+    url = f'https://palworld.fandom.com/wiki/Univolt'
+    url = url.replace(" ", "+")
+    print(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    print((response.status_code))
+    if response.status_code == 200:
+        html_content = response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        PalStats = soup.find_all('div', class_='pi-data-value pi-font')
+        PalFood = soup.find_all('img',class_='wsgray lazyload')
+        print(PalFood)
+
+        print(PalStats[3])
+        palelement1 = ''
+        palelement2 = ''
+        palelements = []
+        paldrop1 = ''
+        paldrop2 = ''
+        paldrop3 = ''
+        paldrops = []
+        foodneed=10
+
+        for PalNumber in PalStats[0]:
+            print(PalNumber.text)
+
+        for PalElement in PalStats[1]:
+            palelements.append(PalElement.text)
+            print(palelements)
+
+        try:
+            if len(palelements[0]) > 1:
+                palelement1 = str(palelements[0])
+                palelement1 = palelement1.replace(' ', '')
+        except:
+            palelement1 = None
+
+        try:
+            if len(palelements[2]) > 1:
+                palelement2 = str(palelements[2])
+                palelement2 = palelement2.replace(' ','')
+        except:
+            palelement2 = None
+
+        for PalDrops in PalStats[2]:
+            paldrops.append(PalDrops.text)
+            print(paldrops)
+
+        try:
+            if len(paldrops[0]) > 1:
+                paldrop1 = str(paldrops[0])
+                paldrop1 = paldrop1.replace(' ','')
+        except:
+            paldrop1 = None
+
+
+        try:
+            if len(paldrops[2]) > 1:
+                paldrop2 = str(paldrops[2])
+                paldrop2 = paldrop2.replace(' ', '')
+        except:
+            paldrop2 = None
+
+
+        try:
+            if len(paldrops[4]) > 1:
+                paldrop3 = str(paldrops[4])
+                paldrop3 = paldrop3.replace(' ','')
+        except:
+            paldrop3 = None
+
+        for Palfood in PalFood:
+            print(Palfood)
+            foodneed -= 1
+
+
+
+
+        return {
+                "Name": f'',
+                "PalNumber": f"{PalNumber.text}",
+                "Element 1": f'{palelement1}',
+                "Element 2": f'{palelement2}',
+                "Drop 1": f'{paldrop1}',
+                "Drop 2": f'{paldrop2}',
+                "Drop 3": f'{paldrop3}',
+                "Food Need": f'{foodneed}'
+
+
+            }
+
