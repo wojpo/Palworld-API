af3e759b73d67b580e3e4736e46c78df3b0fda25
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-01-27 21:11:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,12 @@ async def root():
         with open(file_path, 'w') as json_file:
             json.dump(pallist, json_file)
 
-
+@app.get("/PalList")
+async def root():
+    file_path = 'PalList.json'
+    with open(file_path, 'r') as json_file:
+        PalList = json.load(json_file)
+    print(PalList)
 @app.get("/Pal='{Pal}'")
 async def root(Pal: str):
     global PalNumber, PalElement, PalDrops, paldrop1, paldrop2, PalAppereance, Palwyglond, DeckEntry, Palzahowanie, PartnerSkill, PartnerSkillDesc, kindling, planting, handiwork, lumbering, medicine, transporting, watering, electricity, gathering, mining, cooling, farming
